80a27257031058354e2ffd2c78a4b0057dd51e63
WalterResende85 <42590216+WalterResende85@users.noreply.github.com>
2018-12-04 13:47:06 -0200

    Impressao

--- src/Impressao.java
+++ src/Impressao.java
@@ -116,4 +116,32 @@ public class Impressao {
         gv.writeGraphToFile(ImagemGrafo);
         System.out.println(arquivo + ".png FOI GRAVADO");
     }
+
+    void ImprimePrim(Grafo grafo, String arquivo) throws IOException {
+        String g = "graph graphname {";
+        for (Aresta a : grafo.ListaAresta) {
+            String p = a.getA() + "--" + a.getB() + ";";
+            g = g + p;
+        }
+        g = g + "}";
+
+        FileWriter arq = new FileWriter(arquivo + ".txt");
+        PrintWriter gravarArq = new PrintWriter(arq);
+        gravarArq.printf(g);
+        arq.close();
+        System.out.println("Arquivo " + arquivo + ".txt gravado");
+
+        // Criando um objeto da classe responsável por gerar verticeOrigem imagem do grafo
+        GraphView gv = new GraphView();
+        //Lendo verticeOrigem String 
+        gv.readString(g);
+        //Imprimindo verticeOrigem grafo em texto
+
+        System.out.println(gv.getDotSource());
+        //Gerando uma imagem com verticeB nome out.png 
+        File ImagemGrafo = new File(arquivo + ".png");
+        gv.writeGraphToFile(ImagemGrafo);
+        System.out.println(arquivo + ".png FOI GRAVADO");
+
+    }
 }
