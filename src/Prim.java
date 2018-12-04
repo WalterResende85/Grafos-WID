cf4c91a7a49e9c8403602de755c613daa31485d2
Isaac <isaac-silvajf@hotmail.com>
2018-12-04 12:26:54 -0200

    Prim

--- src/Prim.java
+++ src/Prim.java
@@ -2,42 +2,54 @@
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import org.apache.commons.lang3.SerializationUtils;
 
 public class Prim {
 
-public void AlgoritmoPrim(Grafo grafo) throws IOException {
-        Grafo g = grafo;
-        Ordenador X = new Ordenador();                          //Ordenar A pelos valores de peso
-        Collections.sort(g.getListaAresta(), X);
-        Vertice v = g.getGrafo().get(0);
-        Grafo grafoPrim = new Grafo();
-
-        ArrayList<Aresta> listaArestas = new ArrayList<>();
-        ArrayList<Vertice> listaVertices = new ArrayList<>();
-
-        listaVertices.add(v);
-        for (int j = 0; j < g.getListaAresta().size(); j++) {
-            for (int i = 0; i < g.getListaAresta().size(); i++) {
-                Aresta a = g.getListaAresta().get(i);
-                if (listaVertices.contains(a.getA()) && !listaVertices.contains(a.getB())) {
-                    listaVertices.add(a.getB());
-                    listaArestas.add(a);
-                }
-                if (!listaVertices.contains(a.getA()) && listaVertices.contains(a.getB())) {
-                    listaVertices.add(a.getA());
-                    listaArestas.add(a);
+    public void AlgoritmoPrim(Grafo grafo) throws IOException {
+        Grafo g = SerializationUtils.clone(grafo);
+        g.zerarVisitas();
+        ArrayList<Aresta> T = new ArrayList<>();    //conjunto de arestas da árvore geradora mínima
+        ArrayList<Vertice> V = new ArrayList<>();   // conjunto de vértice do grafo original
+        V.addAll(g.grafo);
+        ArrayList<Vertice> B = new ArrayList<>();   // conjunto de vértice da árvore geradora mínima
+        
+        //
+        Integer i = Integer.MAX_VALUE;//infinito
+        Aresta x = new Aresta();
+        //
+        B.add(V.get(0));                  // Um vértice de G 
+        
+        while(B.size() != V.size())       //enquanto B não contém todos os vértices faça
+        {
+            for (Vertice v : B) {
+                for (Aresta a : v.todasAsArestas()) {
+                    if (a.getPeso()<i && a.visitado == false) {
+                        x = a;
+                        i = a.getPeso();
+                        a.visitado = true;
+                    }
                 }
             }
+            i = Integer.MAX_VALUE;
+            for (Vertice v : B) {
+                if (x.getA().equals(v)) {
+                    B.add(x.getB());
+                    T.add(x);
+                    break;
+                }
+                if (x.getB().equals(v)) {
+                    B.add(x.getA());
+                    T.add(x);
+                    break;
+                }
+            }
+            //x== aresta de menor peso dentre os vertice introduzidos
         }
-        for (int i = 0; i < listaVertices.size(); i++) {    //inserir os vertices no novo grafo
-            listaVertices.get(i).setVerticeSemAresta();     //Apagar as antigas aresta
-            grafoPrim.addVertice(listaVertices.get(i));
-        }
-        for (int i = 0; i < listaArestas.size(); i++) {
-            grafoPrim.getVertice(listaArestas.get(i).getOrigem()).addAresta(listaArestas.get(i));  //pega a aresta e insere no vertice certo
-        }
-        //grafo de kruskol completo a partir daqui 
-        Impressao i = new Impressao();
-        i.ImprimeGrafoOrientado(grafoPrim, "grafoPrim");
-    }    
+        Grafo grafoPrim = new Grafo();
+        grafoPrim.addListaArestas(T);
+        Impressao imprime = new Impressao();
+        imprime.ImprimePrim(grafoPrim, "Prim");
+        //i.ImprimeGrafoOrientado(grafoPrim,"grafoPrim");
+    }
 }
